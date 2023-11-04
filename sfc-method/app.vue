d4906ca6ae17ccea7a53fd80f49b20bd4ab048c5
Shimizu Yuya <blanc2756@gmail.com>
2023-11-04 10:50:40 +0900

    sfcの勉強

---
+++ sfc-method/app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const msg = ref("まだ")
+const onButtonClick = (label: string, event: Event): void => {
+  const target = event.target as HTMLButtonElement
+  const text = target.innerText
+  msg.value = `${label}と${text}`
+}
+</script>
+<template>
+  <p>{{ msg }}</p>
+  <button v-on:click="onButtonClick('Hello', $event)">こんにちは</button>
+</template>
